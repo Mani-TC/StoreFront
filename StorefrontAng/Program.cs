13ebe9c99df76177474a1e7b19d7c17adf92fd5a
TRUNET\manikandch <mani.tc@tru-test.co.nz>
2021-02-19 11:44:14 +1300

    inception

---
+++ StorefrontAng/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace StorefrontAng
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("starting application");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog((ctx, cfg) =>
+                {
+                    cfg
+                        .ReadFrom.Configuration(ctx.Configuration)
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                        .MinimumLevel.Override("System", LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        .Enrich.WithProperty("Environment", ctx.HostingEnvironment.EnvironmentName);
+
+                    if (ctx.HostingEnvironment.IsDevelopment())
+                    {
+                        cfg.WriteTo.Console(
+                            outputTemplate:
+                            "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                            theme: AnsiConsoleTheme.Code);
+                    }
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ StorefrontAng/readme.md
@@ -0,0 +1,15 @@
+﻿* Install autofac
+	- install-package autofac
+	- install-package Autofac.Extensions.DependencyInjection
+	- set UseServiceProviderFactory in program
+* Install serilog
+	- install-package serilog
+	- install-package Serilog.Extensions.Logging
+	- install-package Serilog.Sinks.Console
+	- install-package Serilog.Extensions.Hosting (for hostbuilder)
+	- configure environment specific setting inside Program.UseSerilog in hostbuilder
+* Add authentication
+* Add swagger
+* Add dbContext
+
+* middleware
